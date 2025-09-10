8dab370f515b1fe7bbb8e1af79149ebac8403384
Júlio César Prado <127704877+dvjuliocesar@users.noreply.github.com>
2025-09-10 15:16:34 -0300

    Update util.py

--- util.py
+++ util.py
@@ -124,80 +124,10 @@ class ProcessosAnalisador:
                 
         return estatisticas
     
-   # Criar tabela para gráfico
-    
-    def plotar_graficos(self, comarca_selecionada, ano_selecionado):
-            
-            # Gera gráficos para os dados filtrados por ano  
-        
-
-            # Filtrar dados pela comarca e ano
-            df_comarca = self.df[(self.df['comarca'] == comarca_selecionada)]
-            df_ano = df_comarca[df_comarca['data_distribuicao'].dt.year == ano_selecionado]
-
-            # Agrupar por nome_area_acao
-            estatisticas = df_ano.groupby(['nome_area_acao', 'serventia']).agg(
-                Distribuídos=('data_distribuicao', 'count'),  # Quantidade de datas distribuídas
-                ).reset_index()
-
-            # Calcular baixados no ano
-            baixados_no_ano = df_comarca[
-                (df_comarca['data_baixa'].dt.year == ano_selecionado) & 
-                (df_comarca['data_baixa'].notna())
-            ]
-            
-            # Agrupar baixados da mesma forma que os distribuídos
-            baixados_por_area = baixados_no_ano.groupby(['nome_area_acao', 'serventia']).size()
-            
-            # Mesclar corretamente com estatisticas
-            estatisticas = estatisticas.merge(
-                baixados_por_area.rename('Baixados'), 
-                on=['nome_area_acao', 'serventia'], 
-                how='left'
-            ).fillna(0).astype({'Baixados': 'int'})
-            
-            # Calcular pendentes
-            pendentes_por_area = df_ano[df_ano['data_baixa'].isna()].groupby(
-                ['nome_area_acao', 'serventia']
-            ).size()
-        
-            estatisticas = estatisticas.merge(
-                pendentes_por_area.rename('Pendentes'), 
-                on=['nome_area_acao', 'serventia'], 
-                how='left'
-            ).fillna(0).astype({'Pendentes': 'int'})
-            
-            # Calcular total de processos Baixados e Pendentes
-            estatisticas['Total Baixados'] = estatisticas['Baixados'].sum()
-            estatisticas['Total Pendentes'] = estatisticas['Pendentes'].sum()      
-
-            # Calcular taxa de congestionamento
-            estatisticas['Taxa de Congestionamento (%)'] = (
-                (estatisticas['Total Pendentes'] / (estatisticas['Total Pendentes'] + estatisticas['Total Baixados'])) * 100
-                ).round(2)
-
-            #
-            fig = px.bar(
-                estatisticas,
-                x="nome_area_acao",
-                y=["Distribuídos", "Baixados", "Pendentes"],
-                barmode="group",
-                title=f"Comarcas X Taxa de Congestionamento (%) X Área de Ação - {ano_selecionado}",
-                labels={"value": "Quantidade", "variable": "Tipo"}
-            )
-
-            # Adicionar indicadores
-            Distribuidos = estatisticas["Distribuídos"].sum()
-            Baixados = estatisticas["Baixados"].sum()
-            Pendentes = estatisticas["Pendentes"].sum()
-            Taxa_Cong = round(estatisticas["Taxa de Congestionamento (%)"].mean(), 2)
-
-            # Retornar o gráfico para visualização posterior
-            return fig, Distribuidos, Baixados, Pendentes, Taxa_Cong
-    
+    # Gráfico de Barras Filtrados por Ano:
     def plotar_graficos_ano(self, ano_selecionado): 
 
-        df_grafico = pd.read_csv('uploads\dados_je_geral_25042025.csv', sep=',', low_memory=False)
+        df_grafico = self.df
         df_grafico = df_grafico[['processo_id','nome_area_acao', 'comarca', 'data_distribuicao', 'data_baixa']]
         df_grafico['data_distribuicao'] = pd.to_datetime(df_grafico['data_distribuicao'], errors='coerce')
         df_grafico['data_baixa'] = pd.to_datetime(df_grafico['data_baixa'], errors='coerce')
@@ -220,27 +150,13 @@ class ProcessosAnalisador:
         taxa_cong = taxa_cong.fillna(0).round(2)
         taxa_cong = taxa_cong.reset_index(name='Taxa de Congestionamento (%)')
         taxa_cong['Taxa de Congestionamento (%)'] = taxa_cong['Taxa de Congestionamento (%)'].astype(float)
-
-        # Taxa de congestionamento para juizados
-        taxa_cong = taxa_cong[(taxa_cong['nome_area_acao'].str.contains('civel')) | 
-                                       (taxa_cong['nome_area_acao'].str.contains('criminal')) | 
-                                       (taxa_cong['nome_area_acao'].str.contains('infancia e juventude civel')) |
-                                       (taxa_cong['nome_area_acao'].str.contains('infancia e juventude infracional')) |
-                                       (taxa_cong['nome_area_acao'].str.contains('fazenda publica mista')) |
-                                       (taxa_cong['nome_area_acao'].str.contains('juizado especial fazenda publica'))]
-                                                                                
-        # Mapa de cores personalizado (exemplo para 2 comarcas)
-        '''color_map = {
-            'juizado especial civel': 'darkblue',
-            'juizado especial criminal': 'lightblue'    
-        }'''
         
         # Adiciona uma coluna com rótulo formatado
         taxa_cong['label_text'] = taxa_cong[
             'Taxa de Congestionamento (%)'].apply(lambda x: f'{x:.2f}%')
 
         # Gráfico de barras
-        fig = px.bar(
+        fig_barras = px.bar(
             taxa_cong,
             x='comarca',
             y=['Taxa de Congestionamento (%)'],
@@ -253,17 +169,17 @@ class ProcessosAnalisador:
         )
         
         # Força os rótulos a aparecerem dentro ou fora, conforme necessário
-        fig.update_traces(
+        fig_barras.update_traces(
             textposition='auto',  # Plotly decide o melhor lugar
             textfont=dict(size=12, color='black'),  # você pode ajustar
             insidetextanchor='start'
         )
 
-        fig.update_layout(
+        fig_barras.update_layout(
             yaxis_title='Taxa de Congestionamento (%)',
             uniformtext_minsize=8,
             uniformtext_mode='show',
             legend_title_text='Área de Ação por Comarca'
         )
-        return fig
+        return fig_barras
     
